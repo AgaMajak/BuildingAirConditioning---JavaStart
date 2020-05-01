ffe202952f9a712e74eabe8b13bd17ba33799d6b
Agnieszka Majak <agamajak@wp.pl>
2020-05-01 15:10:12 +0200

    added method to turn Conditioner on

--- src/Room.java
+++ src/Room.java
@@ -1,14 +1,35 @@
 public class Room {
     double temperature;
     String roomNumber;
-
+    double roomVolume;
     AirConditioner airConditioner;
 
-    void conditionerOn(){
-        if (airConditioner instanceof BasicAirConditioner){
+    public Room(double temperature, String roomNumber, double roomVolume, AirConditioner airConditioner) {
+        this.temperature = temperature;
+        this.roomNumber = roomNumber;
+        this.roomVolume = roomVolume;
+        this.airConditioner = airConditioner;
+    }
 
+    void conditionerOn() {
+        if (airConditioner instanceof BasicAirConditioner) {
+            if (temperature > airConditioner.maxTemperature) {
+                temperature -= 1.0 / roomVolume;
+            } else {
+                System.out.println("Nie udało się obniżyć temperatury, osiągnięto temperaturę minimalną.");
+            }
+        } else if (airConditioner instanceof ProAirConditioner) {
+            if (temperature > airConditioner.maxTemperature) {
+                temperature -= 2.0 / roomVolume;
+            } else {
+                System.out.println("Nie udało się obniżyć temperatury, osiągnięto temperaturę minimalną.");
+            }
         }
     }
 
-
+    @Override
+    public String toString() {
+        return "Pokój numer: " + roomNumber +
+                ". Aktualna temperatura: " + temperature;
+    }
 }
