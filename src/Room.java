c9333e1902d2e93b5488b5f8dbf5ad5532044bb8
Agnieszka Majak <agamajak@wp.pl>
2020-05-01 15:30:13 +0200

    added room array and loops for using conditionerOn method

--- src/Room.java
+++ src/Room.java
@@ -1,3 +1,5 @@
+import javax.crypto.spec.PSource;
+
 public class Room {
     double temperature;
     String roomNumber;
@@ -13,23 +15,22 @@ public class Room {
 
     void conditionerOn() {
         if (airConditioner instanceof BasicAirConditioner) {
-            if (temperature > airConditioner.maxTemperature) {
-                temperature -= 1.0 / roomVolume;
+            if (temperature > airConditioner.maxTemperature+(1.0/roomVolume)){
+            temperature -= 1.0 / roomVolume;
             } else {
                 System.out.println("Nie udało się obniżyć temperatury, osiągnięto temperaturę minimalną.");
             }
         } else if (airConditioner instanceof ProAirConditioner) {
-            if (temperature > airConditioner.maxTemperature) {
+            if (temperature > airConditioner.maxTemperature+(2.0/roomVolume)){
                 temperature -= 2.0 / roomVolume;
             } else {
-                System.out.println("Nie udało się obniżyć temperatury, osiągnięto temperaturę minimalną.");
+                System.out.printf("Nie udało się obniżyć temperatury w pokoju %s, osiągnięto temperaturę minimalną.\n", roomNumber);
             }
         }
     }
 
     @Override
     public String toString() {
-        return "Pokój numer: " + roomNumber +
-                ". Aktualna temperatura: " + temperature;
+        return String.format("Pokój numer: %s. Aktualna temperatura: %.1f", roomNumber, temperature);
     }
 }
--- src/RoomTest.java
+++ src/RoomTest.java
@@ -1,2 +1,33 @@
 public class RoomTest {
+    public static void main(String[] args) {
+        AirConditioner airConditionerBasic = new BasicAirConditioner(22.0);
+        AirConditioner airConditionerPro = new ProAirConditioner(18.0);
+
+        Room[] rooms = {
+                new Room(22.5, "202A", 5.0, airConditionerBasic),
+                new Room(25, "15", 10, airConditionerPro),
+                new Room(19.0, "311", 20, airConditionerPro)
+        };
+
+        for (Room room : rooms) {
+            System.out.println(room);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            rooms[0].conditionerOn();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            rooms[1].conditionerOn();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            rooms[2].conditionerOn();
+        }
+
+        for (Room room : rooms) {
+            System.out.println(room);
+        }
+
+    }
 }
